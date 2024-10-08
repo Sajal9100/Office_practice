227931a80879679a81f54ac1bdc7a4dcd5ec27da
Sajal9100 <114485439+Sajal9100@users.noreply.github.com>
2024-10-08 09:32:38 +0000

    Converstion Done

---
+++ Basics/Converstion.js
@@ -0,0 +1,31 @@
+// let scoure = "33"
+
+// console.log(typeof scoure)
+// console.log(typeof (scoure))
+
+// let ValueInNumber = Number(scoure)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
+
+
+//"33" => 33
+// But
+//"33abc" => Not a number(NAN)
+// TRUE =>  1
+// FALSE => 0
+
+// let isloggesdin = 1
+// let booleanisloggedin = Boolean(isloggesdin)
+
+// console.log(booleanisloggedin);
+
+//1 =>
+// "" => false
+// "sajal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log( typeof stringNumber)
