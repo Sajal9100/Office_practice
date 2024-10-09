efee2461b540251be128d5d66abda69b3601973a
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-09 07:15:08 +0000

    lecture_7_Done

---
+++ Basics/operation.js
@@ -0,0 +1,22 @@
+let str1 = "Hello"
+let str2 = " Sajal"
+
+let str3 = str1+str2
+
+console.log(str3)
+
+//example 2
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+// Java script mai jb ham consol.log comand run krte hai to jo bhi data type sbse agey hota hai 
+// rest of the value bhi usi ke accourding threat hoti hai 
+// like in line 12 pehli value "1" jo ki ek string hai isliye is ka output 122 aya
+// and in line 13 pehli value interger thi toh strings se pehley tk ki value add ho gyo and string as it is print ho gyi
+
+let counterscoure = 100
+counterscoure++
+console.log(counterscoure)
