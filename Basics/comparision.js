3e5567ae9ab8a0b98f146d0ced41a73f4b163106
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-09 09:42:25 +0000

    lecture_8_done

---
+++ Basics/comparision.js
@@ -0,0 +1,12 @@
+// When we compare different type of data
+
+console.log("1" > 4)
+
+//it will give correct ans
+//java script allow programer to compare two different type of data
+//While in Type script programmer are not allowe to do so.
+console.log(null> 0)
+console.log(null>= 0)
+console.log(null == 0)
+
+// "===" this comand also check's the data type.
