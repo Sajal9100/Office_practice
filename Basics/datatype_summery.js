5c62888f9ce9d124af85c411cd3cacf9cffac7a5
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-10 09:06:10 +0000

    lecture_9_done

---
+++ Basics/datatype_summery.js
@@ -0,0 +1,19 @@
+// Java-script is a dynamically typed lamguage
+// primitive data type
+
+// 7 types:- string, int, boolean, null, undefined, bigint, synbol
+
+// Symbol:-
+
+const id = "1234"
+const anotherid = "123"
+
+console.log(id === anotherid)
+
+// ************************************************************************************************************************************
+
+// by using symbol data type we can assign same value to different constant
+
+// Refrance or non-primitive
+// Array, objects, functions
+
