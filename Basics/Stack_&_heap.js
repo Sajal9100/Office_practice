414ab24fe8a71b6e38105fa21f8d29e523d1c1dc
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-11 09:06:09 +0000

    lecture_10_incomplete

---
+++ Basics/Stack_&_heap.js
@@ -0,0 +1,12 @@
+    // //                              Types of memory                           
+    // //                                     |
+    // //     ________________________________|__________________________________
+    // //     |                                                                  |
+    // //   Stack                                                               Heap
+    // (Used by primitive datatypr)                                  (used by non-primitivw datatype)
+
+    let myNameis = 'Sajal'
+    let anotherNmae = myNameis
+    anotherNmae = 'Sajal singh'
+
+    console.log(anotherNmae)
