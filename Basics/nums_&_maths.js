54a45efa115c575123c182bcc424b8a99c990d3e
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-14 10:57:44 +0000

    lecture_12_Maths_pending

---
+++ Basics/nums_&_maths.js
@@ -0,0 +1,12 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1));
+
+const othernumber = 123.399
+
+// console.log(othernumber.toPrecision(4))
