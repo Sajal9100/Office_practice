ced7a7525a004cf9a4bf4604da58906b45b932c7
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-14 10:13:35 +0000

    lecture_11_done

---
+++ Basics/String.js
@@ -0,0 +1,9 @@
+let name = "Sajal singh"
+let repoCount = 5
+
+// console.log(name + repoCount + " value") -> old technice 
+
+console.log(`hello my name is ${name} and i have ${repoCount} repo in my github account `)
+
+console.log(name.length)
+console.log(name.toUpperCase())
