d410c3be096c0268d4c9f82e83a27f0a16c035bf
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-19 12:35:35 +0000

    streek

---
+++ Basic_02/Objects.js
@@ -0,0 +1,16 @@
+// Singleton
+// object.create
+
+
+//  Object literls
+const JS_USER = {
+    name: "sajal",
+    fullname: "Sajal singh"
+}
+
+// ways to accese
+// 1,
+   
+ console.log(JS_USER.fullname,JS_USER.name);
+
+   
