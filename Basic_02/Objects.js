a3df6ae14933c398240ee843de5bee5ce913b9ce
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-21 08:12:26 +0000

    Lecture 16 done

--- Basic_02/Objects.js
+++ Basic_02/Objects.js
@@ -1,16 +1,31 @@
 // Singleton
-// object.create
+// object.create = way to decliar singleton object 
 
 
 //  Object literls
+
+let mysmy = Symbol("Mykey one")
 const JS_USER = {
     name: "sajal",
-    fullname: "Sajal singh"
+    fullname: "Sajal singh",
+    mysmy: 'MyKey one'
 }
 
 // ways to accese
 // 1,
    
- console.log(JS_USER.fullname,JS_USER.name);
+console.log(JS_USER.mysmy);
+
+
+JS_USER.greeting = function () {
+    console.log('Hello Js_User');
+}
+
+JS_USER.greeting = function () {
+    console.log(`Hello Js_User, ${this.fullname}`);
+}
+console.log(JS_USER.greeting());
+
+
 
    
