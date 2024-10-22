46cb71dd6822bfd327196fb95644284e9b253950
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-22 10:48:38 +0000

    lecture 19

---
+++ Functions/1.js
@@ -0,0 +1,22 @@
+function SayMyName () {
+    console.log('s');
+    console.log('a');
+    console.log('j');
+    console.log('a');
+    console.log('l');   
+}
+
+// SayMyName()
+
+// function addTwoNumbers (number1 , number2){
+//     console.log(number1 + number2);    
+// }
+
+function addTwoNumbers (number1 , number2){
+    let result = number1 + number2
+    return result   
+}
+
+const result = addTwoNumbers(3,4)
+
+console.log("Result: " , result);
