9ae5176f91c8eadc24b17ac210c3c50717ad274a
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-22 08:08:10 +0000

    leacture 18 done

--- Basic_02/Object_2.js
+++ Basic_02/Object_2.js
@@ -2,3 +2,22 @@
 
 const tinderUser = {}
 //its a non singleton object
+tinderUser.id = "123abc"
+tinderUser.name = "Sajal singh"
+tinderUser.isLoggedIN = false
+
+// console.log(tinderUser.name)
+
+const regularuser = {
+   email: 'some@gmail.com',
+   fullname: {
+    Userfullname:{
+        firstName:"Sajal",
+        LastName: "Singh"
+    }
+   }
+
+}
+console.log(regularuser.Userfullname.firstName);
+
+
