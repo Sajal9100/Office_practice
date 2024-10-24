e96c697cb23a9987a916a239593138b974c5420b
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-24 17:34:11 +0000

    done

--- Functions/1.js
+++ Functions/1.js
@@ -12,11 +12,23 @@ function SayMyName () {
 //     console.log(number1 + number2);    
 // }
 
-function addTwoNumbers (number1 , number2){
-    let result = number1 + number2
-    return result   
+// function addTwoNumbers (number1 , number2){
+//     let result = number1 + number2
+//     return result   
+// }
+
+// const result = addTwoNumbers(3,4)
+
+// console.log("Result: " , result);
+
+function loginUserMessage(Username) {
+    if (Username === undefined) {
+        console.log("Please enter a valid username")
+        return
+    }
+
+    return `${Username} just logged in`
 }
 
-const result = addTwoNumbers(3,4)
-
-console.log("Result: " , result);
+console.log
+ (loginUserMessage())
---
+++ Functions/2.js
@@ -0,0 +1,6 @@
+function addtocartprice(...num1) {
+    return num1
+}
+
+console.log(addtocartprice(200,400,500));
+
