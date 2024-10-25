2206a3e4b36eb6585e02887554081e145427c30e
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-25 09:04:21 +0000

    lecture 22

---
+++ Scopes/1.js
@@ -0,0 +1,16 @@
+function One() {
+    const username = 'Sajal singh'
+    function two () {
+        const website = 'youtube'
+        console.log(username);        
+    }
+    // console.log(website);  This line is error 
+    two()    
+}
+
+One()
+
+
+if (condition) {
+    
+}
