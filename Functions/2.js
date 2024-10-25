ed913243bd826c886301ece1eaea5b08c4bd5661
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-25 07:23:54 +0000

    lecture 20

--- Functions/2.js
+++ Functions/2.js
@@ -4,3 +4,19 @@ function addtocartprice(...num1) {
 
 console.log(addtocartprice(200,400,500));
 
+
+const user = {
+   username: 'Sajal singh',
+   prise: 1500
+}
+
+function handleobject (anyobject)  {
+    console.log(`Username is ${anyobject.username} and prise is ${anyobject.prise}`);
+    
+}
+
+// handleobject(user)
+handleobject({
+    username: "Saloni",
+    prise: 1600
+})
