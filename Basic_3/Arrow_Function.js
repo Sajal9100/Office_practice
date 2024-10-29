9c08bb6ee8bb0b34eae5c322d4e254a82efafdaa
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-29 07:15:00 +0000

    upto Lecture 24

--- Basic_3/Arrow_Function.js
+++ Basic_3/Arrow_Function.js
@@ -1,12 +1,26 @@
-const user = {
-    username:'Sajal singh',
-    price: 999,
+// const user = {
+//     username:'Sajal singh',
+//     price: 999,
 
-    welcomeMessage:  function (){
-        console.log(`${this.username} ,Welcome to this website`);
-    }
+//     welcomeMessage:  function () {
+//         console.log(`${this.username} ,Welcome to this website`);
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "Rishi"
+// user.welcomeMessage( )
+
+
+const chai = () => {
+    let username = "Sajal"
+    console.log();    
 }
 
-user.welcomeMessage()
-user.username = "Rishi"
-user.welcomeMessage()
+chai()
+
+// this is basic arrow function
+
+const coffee = (num1,num2) =>  num1 + num2;
+
+console.log(coffee(3 , 4));
